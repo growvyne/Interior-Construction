7963021db139d4dba8236a944a33bb17c8655575
growvyne <225375993+growvyne@users.noreply.github.com>
2025-08-22 17:39:27 +0530

    update  your counting

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -90,7 +90,7 @@ const CounterSection = () => {
                 mb: 1,
               }}
             >
-              <CountUp end={item.number} duration={6} />
+              <CountUp end={item.number} duration={10} />
             </Typography>
             <Typography
               sx={{
